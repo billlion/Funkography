d0f4220373aa66aa95bd637569567a4ddb8c03eb
Bill Lionheart <bill.lionheart@manchester.ac.uk>
2024-02-09 22:37:53 +0000

    First working version with sparse matrix A

--- FunkMakeA.m
+++ FunkMakeA.m
@@ -3,26 +3,23 @@ function A = FunkMakeA(FP)
 %   A linear forward problem based operator on integrating over circle
 %   Makes a sparse matrix, for a problem assumed small rather than
 %   computing on the fly
-A= sparse(length(FP.Radii)*length(FP.Thetas),FP.PixelsXY^2);
-
-Xs=linspace(-sqrt(2)*FP.ImageMaxRadius,sqrt(2)*FP.ImageMaxRadius,FP.PixelsXY);
-
-[X,Y]=meshgrid(Xs,Xs);%Maybe should be midpoints
+FP=InitFP(FP);
 thismeas=1;
 for r=FP.Radii
-  for th=FP.Thetas
+  for th=FP.Thetas 
       [x0,y0]=pol2cart(th,FP.OuterRadius);
       SrcPt=[x0*ones(1,FP.PixelsXY^2);y0*ones(1,FP.PixelsXY^2)];
-      [rw,col,v]=find(norm([X(:),Y(:)]-SrcPt')<=r+FP.Inc/sqrt(2)&...
-          norm([X(:),Y(:)]-SrcPt')>=r-FP.Inc/sqrt(2));
+      [rw,col,v]=find(...
+            sum(([FP.X(:),FP.Y(:)]-SrcPt').^2,2)<=(r+FP.Inc/sqrt(2))^2 ... 
+          & sum(([FP.X(:),FP.Y(:)]-SrcPt').^2,2)>=(r-FP.Inc/sqrt(2))^2) ;
       if ~isempty(v)
-        Arow=sparse(rw,col,v);
-        A(thismeas,:)=Arow(:);
+        Arow=sparse(rw,col,v,FP.PixelsXY.^2,1);
+        FP.A(thismeas,:)=Arow;
       end
       thismeas=thismeas+1;
   end
 end
-
+A=FP.A;
 end
 
 
--- TestFunk.m
+++ TestFunk.m
@@ -1,6 +1,6 @@
 %Test the Funk
 FunkParams.OuterRadius= 3;  % the circle of the centres of the circles
-                            % overwich we integrate
+                            % overwhich we integrate
 FunkParams.ImageMaxRadius = 1;
 %  The square image has this many pixels
 FunkParams.PixelsXY = 20;
@@ -11,3 +11,7 @@ FunkParams.Radii= linspace(FunkParams.OuterRadius - FunkParams.ImageMaxRadius,..
                    FunkParams.OuterRadius + FunkParams.ImageMaxRadius,NRadii);
 FunkParams.Inc= sqrt(2)*FunkParams.ImageMaxRadius/FunkParams.PixelsXY;
 A = FunkMakeA(FunkParams);
+figure,spy(A);
+for i=[70, 170,280,320]
+    figure, imagesc(reshape(A(i,:),20,20));
+end
